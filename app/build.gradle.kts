1d1b367b66c252e227447f35abc8c119bf8be1c8
Punyo <49717092+Punyo@users.noreply.github.com>
2024-12-10 03:07:41 +0900

    chore:minSdkを26に変更する

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.punyo.moodlefunctioncaller"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
